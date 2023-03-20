47ff56a5a6079b4a581cb74959b34393b3c6caf6
Raghavendra M J <raghavendramj123@gmail.com>
2023-03-20 21:19:49 +0530

    Array Add / Remove methods

--- JavaScript/basics/09_ArrayMethods.js
+++ JavaScript/basics/09_ArrayMethods.js
@@ -8,21 +8,36 @@ function arrayExample() {
   for (var eachName of arr1) {
     console.log("eachName -> ", eachName);
   }
-
   //Accessibility
   console.log("First Element :- ", arr1[0]);
   console.log("Last Element :- ", arr1[arr1.length - 1]);
 }
 
-arrayExample();
+// arrayExample();
 
 function addRemoveElementsFromEnd() {
   var people = ["Siman", "Virat"];
-  console.log("************* Before Push :- ", people);
-  people.push("Rishabh");
-  people.push("Praneeth");
-  console.log("************* After Push :- ", people);
+  console.log("******** Before Push :- ", people);
+  people.push("Rishabh"); //Add Element at the end
+  people.push("Praneeth"); //Add Element at the end
+  console.log("******** After Push :- ", people); 
+  people.pop();
+  people.pop();
+  console.log("******** After Poping 2 elements :- ", people);
 }
 
-addRemoveElementsFromEnd();
+// addRemoveElementsFromEnd();
 
+function addRemoveElementsFromBeginning(){
+  var people = ["Rishabh", "Praneeth"];
+  console.log("******** Before Unshifting/Adding :- ", people);
+  people.unshift("John"); //Add Element at the front
+  people.unshift("Adam"); //Add Element at the front
+  people.unshift("Mathew"); //Add Element at the front
+  console.log("******** Before Shifting/Removing :- ", people); 
+
+  people.shift(); //Removes element from the front
+  people.shift(); //Removes element from the front
+  console.log("******** Aftr Shifting/Removing (2) :- ", people); 
+}
+addRemoveElementsFromBeginning();
