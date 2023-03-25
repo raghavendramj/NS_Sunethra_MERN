5af25ab1c2b2497edd231d58ca6eb9b51423fad5
Raghavendra M J <raghavendramj123@gmail.com>
2023-03-25 22:23:46 +0530

    Getters and setters

--- JavaScript/oops/02_Abstraction.js
+++ JavaScript/oops/02_Abstraction.js
@@ -1,9 +1,15 @@
 //Constructor function
 function Product(price, brand, name, qty) {
+  //You must attach all the members to this to make it visible to outside world
   this.price = price;
   this.brand = brand;
   this.name = name;
   this.qty = qty;
+  this.getProductInfo = function () {
+    productInfo();
+    var stockAfterPurchase = Math.abs(this.qty - availableStock);
+    console.log("Available stock is after the order is ", stockAfterPurchase);
+  };
 
   //Private variable - Hide this from outside world
   var availableStock = 10;
@@ -14,12 +20,10 @@ function Product(price, brand, name, qty) {
     var info = `${name} of brand ${brand} having quantity ${qty} is worth ${finalPrice}`;
     console.log(info);
   };
-  this.getProductInfo = function () {
-    productInfo();
-    var stockAfterPurchase = Math.abs(this.qty - availableStock);
-    console.log("Available stock is after the order is ", stockAfterPurchase);
-  };
 }
 
 var samsungMobile = new Product(12000, "Samsung", "M15", 3);
 samsungMobile.getProductInfo();
+
+console.log("samsungMobile.availableStock :- ", samsungMobile.availableStock); //undefined
+samsungMobile.productInfo(); //TypeError: samsungMobile.productInfo is not a function
---
+++ JavaScript/oops/03_GettersSetters.js
@@ -0,0 +1,33 @@
+//Constructor function
+function Product(price, brand, model) {
+  this.price = price;
+  this.brand = brand;
+  this.model = model;
+
+  //Private variable
+  var stocks = 10;
+
+  Object.defineProperty(this, "stks_avail", {
+    //Getter for stocks
+    get: function () { 
+      return stocks;
+    },
+    set: function (value) {
+      if (value === "" || value === "undefined" || value === undefined) {
+        throw Error("Invalid stock information");
+      }
+
+      if (typeof value !== "number") {
+        throw Error("Invalid stock information - Its not a number");
+      }
+      console.log("Adding new ", value, "stocks !");
+      stocks += value;
+    },
+  });
+}
+
+var apple = new Product(85000, "Apple", "IPhone14");
+console.log("apple ->", apple);
+console.log("1. apple.stks_avail ->", apple.stks_avail);
+apple.stks_avail = 12;
+console.log("2. apple.stks_avail ->", apple.stks_avail);
