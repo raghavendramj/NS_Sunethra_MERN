f1fa245720d581117bdcece661899be4bb01d93f
Raghavendra M J <raghavendramj123@gmail.com>
2023-04-04 22:02:38 +0530

    Hoisting

--- JavaScript/es6_features/01_keywords.js
+++ JavaScript/es6_features/01_keywords.js
@@ -18,4 +18,29 @@ function testLetKeyword() {
   }
   console.log("3. Number :- ", number);
 }
-testLetKeyword();
+// testLetKeyword();
+
+function hoistingVarTest() {
+  console.log("1. Hoisting :: Number :- ", number);
+  var number;
+  console.log("2. Hoisting :: Number :- ", number);
+  number = 10;
+  console.log("3. Hoisting :: Number :- ", number);
+}
+
+hoistingVarTest();
+
+function addNumbers() {
+  debugger;
+  console.log("A:-", a);
+  console.log("Addition funtion");
+  var b;
+  console.log("B:-", b);
+  var a;
+  console.log("C:-", c);
+  a = 10;
+  b = 20;
+  var c = a + b;
+  console.log("Finally C :-", c);
+}
+addNumbers();
