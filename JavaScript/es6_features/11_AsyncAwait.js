e8bd90def9d23b7582c5956d3311565b09ad8f67
Raghavendra M J <raghavendramj123@gmail.com>
2023-04-11 22:06:08 +0530

    Async and await!

---
+++ JavaScript/es6_features/11_AsyncAwait.js
@@ -0,0 +1,28 @@
+//Returns a promise object
+async function returnNumber() {
+  return 1;
+}
+
+//SAME AS ABOVE, explicit mention Returns a promise object
+async function returnNumberSecond() {
+  return Promise.resolve(1);
+}
+const retValue = returnNumber();
+console.log("ASYNC Test ::  -> ", retValue);
+
+retValue.then(
+  (msg) => console.log("Success :- ", msg),
+  (err) => console.log("Failure :- ", err)
+);
+
+//Await only works inside an async function
+async function testAwait() {
+  let promise = new Promise((resolve, reject) => {
+    setTimeout(() => resolve("Completed!"), 3000);
+  });
+
+  let result = await promise;
+  console.log("AWAIT Test :: Success result :- ", result);
+}
+
+testAwait();
