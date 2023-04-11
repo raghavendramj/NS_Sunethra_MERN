96c9132d82fb5ef7fba2e753c5377346fa883a90
Raghavendra M J <raghavendramj123@gmail.com>
2023-04-11 21:39:22 +0530

    Promise Chaining

---
+++ JavaScript/es6_features/10_PromiseChaining.js
@@ -0,0 +1,72 @@
+const addPromise = (arr) => {
+  return new Promise((resolve, reject) => {
+    if (arr.length < 2) {
+      reject("Unable to perform Addition");
+    } else {
+      let result = arr[0] + arr[1];
+      console.log("Sum is :- ", result);
+      resolve(arr);
+    }
+  });
+};
+
+const subPromise = (arr) => {
+  return new Promise((resolve, reject) => {
+    if (arr.length < 2) {
+      reject("Unable to perform Addition");
+    } else {
+      let a = arr[0];
+      let b = arr[1];
+      let result = a > b ? a - b : b - a;
+      console.log("Difference is :- ", result);
+      resolve(arr);
+    }
+  });
+};
+
+const multiplyPromise = (arr) => {
+  return new Promise((resolve, reject) => {
+    if (arr.length < 2) {
+      reject("Unable to perform Addition");
+    } else {
+      let result = arr[0] * arr[1];
+      console.log("Product is :- ", result);
+      resolve(arr);
+    }
+  });
+};
+
+let arr = [23, 15];
+let firstPromise = addPromise(arr);
+
+//Addition
+let secondPromise = firstPromise.then(
+  (arr) => {
+    console.log("Addition Response :- ", arr);
+    return subPromise(arr);
+  },
+  (err) => {
+    console.log("Failed to execute! ", err);
+  }
+);
+
+//Subtraction
+let thirdPromise = secondPromise.then(
+  (arr) => {
+    console.log("Subtraction Response :- ", arr);
+    return multiplyPromise(arr);
+  },
+  (err) => {
+    console.log("Failed to execute! ", err);
+  }
+);
+
+//Multiplication
+thirdPromise.then(
+  (arr) => {
+    console.log("Last Success Response :- ", arr);
+  },
+  (err) => {
+    console.log("Failed to execute! ", err);
+  }
+);
