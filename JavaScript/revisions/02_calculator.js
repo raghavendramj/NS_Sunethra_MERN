18e5bed2e1eeec40ab2349050b7ea5ed2e2b09aa
Raghavendra M J <raghavendramj123@gmail.com>
2023-04-20 22:03:14 +0530

    IFFE In Action

--- JavaScript/revisions/02_calculator.js
+++ JavaScript/revisions/02_calculator.js
@@ -1,6 +1,12 @@
-function add(a, b) {
-  return a + b;
-}
-function multiply(a, b) {
-  return a * b;
-}
+const calculator = (function () {
+  function add(a, b) {
+    return a + b;
+  }
+  function multiply(a, b) {
+    return a * b;
+  }
+  return {
+    add: add,
+    multiply: multiply,
+  };
+})();
