80aee49aded07020d36ce2979b4e9d900184f2ed
Raghavendra M J <raghavendramj123@gmail.com>
2023-04-27 22:06:17 +0530

    Changing props and state

--- React/reactproject/mynewtonschoolreactapp/src/LifecycleMounting.jsx
+++ React/reactproject/mynewtonschoolreactapp/src/LifecycleMounting.jsx
@@ -7,22 +7,39 @@ class MountingPhaseDemo extends Component {
         console.log("1. constructor called!");
     }
     static getDerivedStateFromProps(props, state) {
-        console.log("2. getDerivedStateFromProps called!");
-        // console.log("2. color before this method is :-  ", state.color);
-        // console.log("2. color received from props is :-  ", props.color);
-        return { color: props.color };
+        if (state.color == 'red') {
+            //During 2 call in sequence 
+            console.log("2. getDerivedStateFromProps called!");
+            console.log("2. color before this method is :-  ", state.color);
+            console.log("2. color received from props is :-  ", props.color);
+            return { color: props.color };
+        } else {
+            console.log("5. getDerivedStateFromProps called!");
+            console.log("5. color before this method is :-  ", state.color);
+            console.log("5. color received from props is :-  ", props.color);
+            //During 5th call in sequence
+            return { color: state.color };
+        }
     }
 
     render() {
-        console.log("3. render called!");
+        if (this.state.color == 'maroon') {
+            console.log("3. render called!"); 
+        } else {
+            console.log("6. render called!");
+        } 
+
         return (
             <h1>Color of my choice is :- {this.state.color}</h1>
         );
     }
     componentDidMount() {
-        console.log("4. componentDidMount called!", this.state.color); 
-        // const newState = { color: 'yellow' };
-        // setTimeout(() => { this.setState(newState); }, 2000);
+        console.log("4. componentDidMount called!", this.state.color);
+        const newState = { color: 'yellow' };
+        setTimeout(() => {
+            console.log("Changing the state.color to yellow");
+            this.setState(newState);
+        }, 2000);
     }
 }
 
