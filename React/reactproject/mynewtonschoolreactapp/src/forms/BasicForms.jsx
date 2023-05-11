8a154b4d6f8db84bcd97e5ff8c12c8ad9685e6e4
Raghavendra M J <raghavendramj123@gmail.com>
2023-05-11 21:58:26 +0530

    Form submit

--- React/reactproject/mynewtonschoolreactapp/src/forms/BasicForms.jsx
+++ React/reactproject/mynewtonschoolreactapp/src/forms/BasicForms.jsx
@@ -4,15 +4,28 @@ function BasicForms() {
 
     const [name, setName] = useState("Enter Your name");
     const nameInput = useRef("");
- 
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(`You have entered ${name} `);
+        alert(`You have entered ${name} `);
+
+    }
+
     return (
         <div className="container mt-3">
             <h1>HTML React Forms</h1>
-            <form>
-                <label htmlFor="Enter your name :- " ></label>
-                <input type="text" ref={nameInput} placeholder={name} onChange={event => setName(event.target.value)} />
+            <form onSubmit={handleSubmit}>
+
+                <div>
+                    <label htmlFor="Enter your name :- " >Enter your name :- </label>
+                    <input type="text" value={name} onChange={event => setName(event.target.value)} />
+                </div>
+                <input type="submit" value="Submit" className="btn btn-primary mt-4" />
             </form>
             <h2>Entered Name :- {name} </h2>
+
+
         </div>
     );
 }
