5d28b2339d3018974720de865c2cce7e1c35d70d
Raghavendra M J <raghavendramj123@gmail.com>
2023-05-13 21:40:26 +0530

    Forms with multiple fields

--- React/reactproject/mynewtonschoolreactapp/src/forms/FormWithMultipleFields.jsx
+++ React/reactproject/mynewtonschoolreactapp/src/forms/FormWithMultipleFields.jsx
@@ -6,16 +6,15 @@ function FormWithMultipleFields() {
 
     const handleChange = (event) => {
         const inputName = event.target.name; //input element's key
-        const inputValue = event.target.value; //input elements's entered value 
-        console.log("inputName :- ", inputName);
-        console.log("inputValue :- ", inputValue);
-        setInputs(values => ({ ...values, [inputName]: inputValue }))
+        //inputs value
+        const inputValue = event.target.type === 'radio' ? event.target.checked : event.target.value;
+        console.log("handleChange :: Entered Values :- ", inputs);
+        setInputs(values => ({ ...values, [inputName]: inputValue }));
     }
     const handleSubmit = (event) => {
         event.preventDefault();
         console.log("Entered Values :- ", inputs);
     }
-
 
     return (
 
@@ -24,12 +23,29 @@ function FormWithMultipleFields() {
             <form onSubmit={handleSubmit}>
                 <table>
                     <tr>
-                        <td><label htmlFor="username">Username :- </label></td>
-                        <td><input type="text" name="username" value={inputs.username || ""} onChange={handleChange} /></td>
+                        <td><label htmlFor="firstName">First Name :- </label></td>
+                        <td><input type="text" name="firstName" value={inputs.firstName || ""} onChange={handleChange} /></td>
+                    </tr>
+                    <tr>
+                        <td><label htmlFor="lastName">Last Name :- </label></td>
+                        <td><input type="text" name="lastName" value={inputs.lastName || ""} onChange={handleChange} /></td>
+                    </tr>
+                    <tr>
+                        <td><label htmlFor="email">Email :- </label></td>
+                        <td><input type="email" name="email" value={inputs.email || ""} onChange={handleChange} /></td>
                     </tr>
                     <tr>
                         <td><label htmlFor="age">Age :- </label></td>
                         <td><input type="text" name="age" value={inputs.age || ""} onChange={handleChange} /></td>
+                    </tr>
+                    <tr>
+                        <td><label htmlFor="phoneNumber">Phone Number :- </label></td>
+                        <td><input type="number" name="phoneNumber" value={inputs.phoneNumber || ""} onChange={handleChange} /></td>
+                    </tr>
+                    <tr>
+                        <td><label htmlFor="phoneNumber">Married Status:- </label></td>
+                        <td><input type="radio" name="married" value="married" checked={inputs.checked} onChange={handleChange} /> Married</td>
+                        <td><input type="radio" name="married" value="unmarried" checked={inputs.checked} /> Un Married</td>
                     </tr>
                     <tr>
                         <input type="submit" className="mt-3 btn btn-primary" value="Submit Form" />
@@ -41,3 +57,5 @@ function FormWithMultipleFields() {
 }
 
 export default FormWithMultipleFields;
+
+//FirstName, lastName, email, phoneNo, Married/Unmarried -> Submit
