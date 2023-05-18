60f178845afa7fafdbeedc3657daad6b754cc2bf
Raghavendra M J <raghavendramj123@gmail.com>
2023-05-18 22:50:35 +0530

    CSS Modules

--- React/reactproject/mynewtonschoolreactapp/src/styles/baiscStyles.jsx
+++ React/reactproject/mynewtonschoolreactapp/src/styles/baiscStyles.jsx
@@ -1,10 +1,10 @@
 import '.././App.css'
-import mycustomstyle from 'example.css'
+import mycustomstyle from './example.module.css'
 
 function BasicStyles() {
     const headerStyle = {
         color: 'blue',
-        backgroundColor: 'lightblueF',
+        backgroundColor: 'lightblue',
         padding: '10px',
         borderRadius: '5px'
     };
@@ -12,7 +12,7 @@ function BasicStyles() {
         <div className="container m-3">
             <h1 style={headerStyle}>Basic Styles</h1>
             <p className="makeitbig">My basic styles</p>
-            <p className={mycustomstyle.makeitsmall}>My basic styles</p>
+            <p className={mycustomstyle.makeitsmall}>My Second styles</p>
         </div>
 
     );
