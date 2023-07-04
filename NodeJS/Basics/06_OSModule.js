b8860bf0745d32729213214ff0cf0aef4130e42d
Raghavendra M J <raghavendramj123@gmail.com>
2023-07-04 22:25:29 +0530

    OS and Events

---
+++ NodeJS/Basics/06_OSModule.js
@@ -0,0 +1,12 @@
+const os = require("os");
+
+
+console.log("os.cpus() :- ", os.cpus());
+console.log("os.arch() :- ", os.arch());
+console.log("os.hostname() :- ", os.hostname());
+console.log("os.homedir() :- ", os.homedir());
+console.log("os.tmpdir() :- ", os.tmpdir());
+console.log("os.uptime() :- ", os.uptime());
+console.log("os.totalmem() :- ", os.totalmem());
+console.log("os.type() :- ", os.type());
+console.log("os.platform() :- ", os.platform());
---
+++ NodeJS/Basics/07_EventModule.js
@@ -0,0 +1,23 @@
+const EventEmitter = require("events");
+
+//Events Flow!
+//1. Define an Event
+//2. Callback function -> What will happen on that event
+//3. Trigger that event
+
+//2. Callback function -> What will happen on that event
+let invokeLogger = (a, b) => {
+  console.log("Log event is triggered!");
+  console.log("Value of A :- ", a);
+  console.log("Value of B :- ", b);
+  let sum = a + b;
+  console.log("Result  :- ",sum);
+};
+
+//1. Define/Linking an Event
+const emitter = new EventEmitter();
+emitter.on("logMessage", invokeLogger);
+
+//3. Triggering the event.
+emitter.emit("logMessage", 10, 20);
+emitter.emit("logMessage", "Arg1", "Arg2");
