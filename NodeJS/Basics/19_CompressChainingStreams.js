f7ac5818a874781d2879b366316b973a86d57604
Raghavendra M J <raghavendramj123@gmail.com>
2023-07-11 21:36:47 +0530

    Zlib introduced

---
+++ NodeJS/Basics/19_CompressChainingStreams.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+const zlib = require("zlib");
+
+//------------- Compression Logic START-------------------
+let readstream = fs.createReadStream("19_FileToRead.txt");
+const zipFileName = "19_Zipped.txt.gz";
+let writeStream = fs.createWriteStream(zipFileName);
+
+readstream.pipe(zlib.createGzip()).pipe(writeStream);
+console.log("File compressed!");
+//------------- Compression Logic END-------------------
+
+//------------- Extraction Logic START-------------------
+//Both started in parallel could cause issues, so setTimeout is introduced to add some delay
+setTimeout(() => {
+  const inpStream = fs.createReadStream(zipFileName);
+  const output = fs.createWriteStream("19_Extracted.txt");
+  inpStream.pipe(zlib.createUnzip()).pipe(output);
+}, 1000);
+//------------- Extraction Logic END-------------------
